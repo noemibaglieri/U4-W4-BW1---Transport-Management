2708e6e3ac94bbe0feae23dc5e785431ebff5fd1
Francesco-Zac <francescozaccaro2@gmail.com>
2025-07-08 16:31:45 +0200

    Update Application

--- src/main/java/noemibaglieri/Application.java
+++ src/main/java/noemibaglieri/Application.java
@@ -1,16 +1,13 @@
 package noemibaglieri;
 
-import noemibaglieri.entities.Maintenance;
+import noemibaglieri.dao.*;
+import noemibaglieri.entities.*;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import noemibaglieri.dao.MaintenanceDAO;
-import noemibaglieri.dao.VehicleDAO;
-import noemibaglieri.entities.Bus;
-import noemibaglieri.entities.Tram;
-import noemibaglieri.entities.Vehicle;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("U4W4BW1");
@@ -19,6 +16,8 @@ public class Application {
         EntityManager em = emf.createEntityManager(); // <-- crei EntityManager
         VehicleDAO vehicleDAO = new VehicleDAO(em);
         MaintenanceDAO maintenanceDAO = new MaintenanceDAO(em);
+
+
 
         // Crea un nuovo Bus
 
@@ -69,6 +68,77 @@ public class Application {
             Maintenance foundMaint = maintenanceDAO.find(manut.getMaintenanceId());
             System.out.println("Maintenance trovata: " + foundMaint);
         }
+
+
+
+        VendorsDAO vd = new VendorsDAO(em);
+        UsersDAO ud = new UsersDAO(em);
+        CardsDAO cd = new CardsDAO(em);
+
+        HumanVendor hv1 = new HumanVendor("Tabaccheria Christian", LocalTime.of(9, 30), LocalTime.of(20,0));
+        HumanVendor hv2 = new HumanVendor("Tabaccheria New Cart", LocalTime.of(7, 30), LocalTime.of(17,0));
+        MachineVendor hv3 = new MachineVendor("Macchinetta Via Dei Volsci", true);
+        MachineVendor mv1 = new MachineVendor("Macchinetta Viale Le Corbusier", true);
+        MachineVendor mv2 = new MachineVendor("Macchinetta Via Tiziano", false);
+
+        // Salva il vendor nel database
+
+        vd.save(hv1);
+        vd.save(hv2);
+        vd.save(hv3);
+        vd.save(mv1);
+        vd.save(mv2);
+
+
+        User user1 = new User("Daenerys", "Targaryen", LocalDate.of(1995,1,14));
+        User user2 = new User("Cersei", "Lannister", LocalDate.of(1978, 9, 11));
+        User user3 = new User("Obara", "Martell", LocalDate.of(2001, 3, 17));
+        User user4 = new User("Olenna", "Tyrell", LocalDate.of(1940, 12, 31));
+        User user5 = new User("Arya", "Stark", LocalDate.of(2004, 12, 11));
+
+
+        ud.save(user1);
+        ud.save(user2);
+        ud.save(user3);
+        ud.save(user4);
+        ud.save(user5);
+
+
+        CardsDAO cd = new CardsDAO(em);//vendors
+        HumanVendor hv1 = new HumanVendor("Tabaccheria Christian", LocalTime.of(9, 30), LocalTime.of(20, 0));
+        HumanVendor hv2 = new HumanVendor("Tabaccheria New Cart", LocalTime.of(7, 30), LocalTime.of(17, 0));
+        MachineVendor hv3 = new MachineVendor("Macchinetta Via Dei Volsci", true);
+        MachineVendor mv1 = new MachineVendor("Macchinetta Viale Le Corbusier", true);
+        MachineVendor mv2 = new MachineVendor("Macchinetta Via Tiziano", false);
+
+
+
+        // recuper0 utenti dal DB
+        User user1 = ud.findById(1L);
+        User user2 = ud.findById(2L);
+        User user3 = ud.findById(3L);
+        User user4 = ud.findById(4L);
+        User user5 = ud.findById(5L);
+
+        // recupero vendors dal DB
+        Vendor vendor1 = vd.findById(1L);
+        Vendor vendor2 = vd.findById(2L);
+        Vendor vendor3 = vd.findById(3L);
+        Vendor vendor4 = vd.findById(4L);
+        Vendor vendor5 = vd.findById(5L);
+
+        // creo cards
+        Card card1 = new Card(LocalDate.of(2024, 7, 1), user1);
+        Card card2 = new Card(LocalDate.of(2024, 8, 1), user2);
+        Card card3 = new Card(LocalDate.of(2024, 6, 15), user3);
+        Card card4 = new Card(LocalDate.of(2024, 5, 20), user4);
+        Card card5 = new Card(LocalDate.of(2024, 7, 5), user5);
+
+        cd.save(card1);
+        cd.save(card2);
+        cd.save(card3);
+        cd.save(card4);
+        cd.save(card5);
 
         em.close();
         emf.close();
